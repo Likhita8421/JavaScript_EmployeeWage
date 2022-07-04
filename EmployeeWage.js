727a51a9f40f6a3778f0e1e695363e47e351a2de
Likhita <leenupandagre24@gmail.com>
2022-07-04 16:47:44 +0530

    Functions

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -41,3 +41,30 @@ const WAGE_PER_HOUR = 20;
     let empWage = emp_Hrs * WAGE_PER_HOUR;
     console.log("UC-2  EmpWage: " + empWage);
 }
+
+
+
+//UC-3 Functions
+const PART_TIME = 1;
+const FULL_TIME = 2;
+const Part_Time_Hours = 4;
+const Full_Time_Hours = 8;
+const Wage_Per_Hour = 20;
+
+    function getWorkingHours(empCheck)
+    {
+        switch (empCheck)
+    {
+        case PART_TIME:
+            return PART_TIME;
+        case FULL_TIME:
+            return FULL_TIME;
+        default:
+            return 0;        
+    }
+    }
+    let empCheck = Math.floor(Math.random() * 10) % 3;
+    let emp_Hrs = getWorkingHours(empCheck);
+    let empWage = emp_Hrs * Wage_Per_Hour;
+    console.log("UC-3  EmpWage: " + empWage + " Hours: "+ emp_Hrs);
+    
