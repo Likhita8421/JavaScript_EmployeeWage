f9f9fb0fa1ba93433f362fc233592d1eb11d8dd5
Likhita <leenupandagre24@gmail.com>
2022-07-07 23:55:52 +0530

    EmpPayroll-ReGex-TryCatch

--- EmployeePayroll.js
+++ EmployeePayroll.js
@@ -1,65 +1,204 @@
-class EmployeePayrollData 
+class EmployeePayrollData
 {
-    //property
-    id;
+    // property
+    id; 
     salary;
     gender;
-    startDate; 
-
-    /// ========constructor====///
-    // constructor(id, name, salary) {
-    //     this.id = id;
-    //     this.name = name;
-    //     this.salary = salary;
-
-    //constructor
-    constructor(...params) {
+    startDate;
+    email;
+    zipCode;
+    
+    constructor(...params)
+    {
         this.id = params[0];
-        this.name = params[1];
+        this.name = params[1]
         this.salary = params[2];
         this.gender = params[3];
         this.startDate = params[4];
-    }
-    // getter & setter method
-    get name()    
-    { 
-        return this._name; 
-    }
-    set name(name) 
-    { 
-        let nameRegex = RegExp('^[A-Z]{1}[a-z]{2,}$');   //......UC13-regex
-        if (nameRegex.test(name))                        //...... UC13
-        //console.log("Setting: "+ name);                //.....used in uc12
-        this._name = name;
-        else throw 'Name is Incorrect!';
+        this.email = params[5];
+        this.zipCode = params[6];
     }
 
-    //method
-    toString() 
+    // getter and setter method
+    get name() 
     {
-        return "id=" + this.id +", name= " + this.name + ",salary= " + this.salary + ", " + "gender = "+ this.gender + ", startDate=" + this.startDate ;
+        return this._name;
+    }
+    set name(name)
+    {
+        this._name = name;
     }
 
-  
-}
+    set name(name)
+    {
+        let nameCheck = RegExp('^[A-Z]{1,}[a-zA-Z]{2,}$')
+        if(nameCheck.test(name))
+        {
+            this._name = name;
+        }
+        else
+        {
+            throw "\n\n-------Raising Exception--------\n\nValiadting the name in given regular expresion to follow the rule : must have \n1.Must have capital letter \n2.Atleast 3 characters\n";
+        }
+        
+    }
+    //Checking Id IS non zero positive number
+
+    get id() 
+    {
+        return this._id;
+    }
+    set id(id)
+    {
+        let idCheck = RegExp('^[1-9]{1}([0-9]*)$')
+        if(idCheck.test(id))
+        {
+            this._id = id;
+        }
+        else
+        {
+            throw "\n\nEmployee ID must have non-zero positive number";
+        }
+    }
+    //Checking Salary Non Positive Number
     
-//UC-1                                                                                                
-let employeePayrollData =new EmployeePayrollData(1, "Mark", 30000);
-console.log("UC-1:"+ employeePayrollData.toString());
+    get salary() 
+    {
+        return this._salary;
+    }
+    set salary(salary)
+    {
+        let salaryCheck = RegExp('^[1-9]{1}([0-9]*)([.]*)[0-9]*$')
+        if(salaryCheck.test(salary))
+        {
+            this._salary = salary;
+        }
+        else
+        {
+            throw "Employee salary must have non zero positive number";
+        }
+    }
 
-//UC-2 Extend empPayroll data Add gender & StartDate
-let newEmployeePayrollData =new EmployeePayrollData(1, "Amit", 40000, "M", new Date());
-console.log("UC-2:"+ newEmployeePayrollData.toString());
+    //Checking Gender M or F
+    get gender() 
+    {
+        return this._gender;
+    }
+    set gender(gender)
+    {
+        let genderCheck = RegExp('^[M|F]{1}$')
+        if(genderCheck.test(gender))
+        {
+            this._gender = gender;
+        }
+        else
+        {
+            throw "Employee gender must have M or F";
+        }
+    }
+    //Checking DAte Not A Future Date
+    get startDate() 
+    {
+        return this._startDate;
+    }
+    set startDate(startDate)
+    {
+        let presentdate = new Date();
+        let dateCheck = RegExp("^[0-9]{1,2}[/][0-9]{1,2}[/][0-9]{4}$");
+        if(startDate <= presentdate.toLocaleDateString())
+        {
+            if(dateCheck.test(startDate))
+            {
+                this._startDate = startDate;
+            }
+            else
+            {
+                throw "Invalid Format";
+            }
+        }
+        else
+        {
+            throw "Employee Start Date will  be less than or equal to present Date";
+        }
+    }
+    
+    //Checking MAil Id With Exact Format
+    get email() 
+    {
+        return this._email;
+    }
+    set email(email)
+    {
+        let emailCheck = RegExp('^[0-9a-z]{1,}([.+-]?)[0-9a-z]{1,}(@)[a-z0-9]{1,}(.)(com|co|net)((.in|.com|.au)*)$')
+        if(emailCheck.test(email))
+        {
+            this._email = email;
+        }
+        else
+        {
+            throw "Invalid Email ID";
+        }
+    }
 
-//UC-13-REgex Pattern & Try-Catch Error
-
- employeePayrollData.name = "Mark";                                           //....Regex Pattern Matching 
- console.log("Uc-3: Pattern Matching:"+ employeePayrollData.toString());
-
-try {
-    employeePayrollData.name = "mark";
-    console.log("Uc-3: TryCatch Error :"+ employeePayrollData.toString());     //...with try-catch will get else part 'Name Is Incorrect' instead of error
+    //Checking ZipCode
+    get zipCode() 
+    {
+        return this._zipCode;
+    }
+    set zipCode(zipCode)
+    {
+        let zipCodeCheck = RegExp('^[0-9]{6}$')
+        if(zipCodeCheck.test(zipCode))
+        {
+            this._zipCode = zipCode;
+        }
+        else
+        {
+            throw "Invalid zipCode";
+        }
+    }
+    
+    // method
+    DisplayEmpDetails()
+    {
+        return "Id: " + this.id + ",\tName: " + this.name + ",\tSalary: " + this.salary + ",\tGender: " + this.gender + ",\tStartDate: " + this.startDate +
+                ",\tEmail: " + this.email + ",\tZip Code: " + this.zipCode;
+    }
 }
-catch (e){
-    console.error(e);
+{
+    try
+    {
+        
+        let empdata = new EmployeePayrollData(1, "Harini", 20000, "F", new Date("11/10/2022").toLocaleDateString(), "abc123@gmail.com", 123456);
+        console.log(empdata.DisplayEmpDetails());
+        empdata = new EmployeePayrollData(2, "Harshini", 25000, "F", new Date("20/12/2021").toLocaleDateString(),"xyz@gmail.com", 789123);
+        console.log(empdata.DisplayEmpDetails());
+        empdata.id = 3;
+        empdata.name = "Chandini";
+        empdata.salary = 20000;
+        empdata.gender = "F";
+        empdata.startDate = new Date("10-12-2022").toLocaleDateString();
+        empdata.email = "abc123@gmail.com";
+        empdata.zipCode = 567234;
+        console.log(empdata.DisplayEmpDetails());
+
+        console.log("\n\n-------------------------Displaying Valid Regex-----------------------------");
+        empdata.id = 2;
+        empdata.name = "Sai"; //The Regex Is Satisfying
+        empdata.salary = 90000;
+        console.log(empdata.DisplayEmpDetails());
+
+        empdata.id = 5;
+        empdata.name = "bai"; //The Regex Is Not Satisfying
+        empdata.salary = 80000;
+        console.log(empdata.DisplayEmpDetails());
+    }
+    catch(ex)
+    {
+        console.error(ex);
+    }
 }
+let empdata2 = new EmployeePayrollData(3,"Harshini",40000,'F',new Date());
+let empdata3 = new EmployeePayrollData(4, "Hardin", 40000,'F',new Date());
+console.log(empdata2.DisplayEmpDetails());
+console.log(empdata3.DisplayEmpDetails());
